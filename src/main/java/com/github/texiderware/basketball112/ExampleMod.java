19b21fd8e8ac392c44064ad435dc8a9f63928b72
GitHub Action <action@github.com>
2025-09-01 14:07:36 +0000

    Initialize template repository proper
    Set package name to com.github.texiderware.basketball112
    Set project name to basketball-1.12
    Set modid to basketball

--- src/main/java/com/github/texiderware/basketball112/ExampleMod.java
+++ src/main/java/com/github/texiderware/basketball112/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.texiderware.basketball112;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "basketball", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
